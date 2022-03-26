e5f5f241278af3a96d6829748c5aa03a11fcc691
gerardobeltran <contacto@gerardobeltran.mx>
2022-03-26 10:34:01 -0700

    --

---
+++ 01 Fundamentos de Programación Competitiva /02 Estructuras de control condicional/F. Fracción mixta/prog.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m, a, b;
+
+    cin >> n >> m;
+
+    a = n / m; // division entera
+
+    b = n % m; // residuo
+
+    if (b==0)
+        cout << a << endl;
+    else
+        cout << a << " " << b << "/" << m << endl;   
+ 
+
+
+    return 0;
+}
