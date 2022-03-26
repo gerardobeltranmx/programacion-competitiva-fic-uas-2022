e76ce6419c1059d8cd77c59d0ed152c06a3381d1
gerardobeltran <contacto@gerardobeltran.mx>
2022-03-26 09:54:17 -0700

    --

---
+++ 01 Fundamentos de Programación Competitiva /02 Estructuras de control condicional/D. Par o impar/prog.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    cin >> numero;
+
+    if (numero % 2 == 0)
+       cout << "par";
+    else
+       cout << "impar";        
+
+
+    return 0;
+}
