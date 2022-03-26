724942940eb33ca6e65bf01fc6336d314715e939
gerardobeltran <contacto@gerardobeltran.mx>
2022-03-26 09:46:40 -0700

    --

---
+++ 01 Fundamentos de Programación Competitiva /02 Estructuras de control condicional/C. Es Mayor o Menor de Edad/prog.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int edad;
+    cin >> edad;
+
+    if (edad>=18){
+       cout << "Es Mayor";
+    }
+    else{
+       cout << "Es Menor";   
+    }
+    return 0;
+}
