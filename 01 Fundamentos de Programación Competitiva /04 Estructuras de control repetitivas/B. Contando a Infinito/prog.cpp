4831a17973f73e4992feb8ca1d9b3a88e05ed02c
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-04-02 09:48:46 -0700

    --

---
+++ 01 Fundamentos de Programación Competitiva /04 Estructuras de control repetitivas/B. Contando a Infinito/prog.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, contador=1;
+    cin >> n;
+
+    while (contador <= n){
+
+        cout << contador << endl;
+        contador++;
+    }
+
+
+    return 0;
+
+}
