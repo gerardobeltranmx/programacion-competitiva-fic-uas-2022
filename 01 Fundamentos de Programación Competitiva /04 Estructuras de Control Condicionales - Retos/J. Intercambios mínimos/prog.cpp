fde68af20f89ce34564557ab6a19629d4769a71f
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-04-02 09:33:01 -0700

    --

---
+++ 01 Fundamentos de Programación Competitiva /04 Estructuras de Control Condicionales - Retos/J. Intercambios mínimos/prog.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    long A, B, C;
+    int N=0;
+
+    cin >> A >> B >> C ;
+
+    if (A > B) {
+
+        if (B > C){
+            N++;
+        }
+        else {
+            if (B < C  and A < C){
+            N++;
+            }
+            else{
+                N+=2;
+            }
+        }
+    }
+    else {
+        if ( B > C and  C > A){
+            N++;
+        }  
+        else if (C<A){
+            N+=2;
+        }
+    }
+
+    cout << N << endl;
+
+    return 0;
+
+}
