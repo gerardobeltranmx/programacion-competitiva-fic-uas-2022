0f7785b90a8a07807ea95c0d64faaa78e475c621
gerardobeltran <contacto@gerardobeltran.mx>
2022-04-30 10:13:29 -0600

    --

---
+++ 01 Fundamentos de Programación Competitiva /05 Arreglos/K. Consecutivos/prog.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+int asc (const void * a, const void * b){
+return ( *(int*)a - *(int*)b );
+}
+int main()
+{
+    int N, i;
+    bool consecutivo=true;
+    cin >> N;
+    int datos[N];
+
+    for (i=0; i<N; i++){
+        cin >> datos[i];
+    }
+    qsort(datos, N, sizeof(int), asc);
+
+    for (i=0; i<N-1 and consecutivo==true; i++){
+        if ( (datos[i]+1) != datos[i+1] )
+                consecutivo=false;       
+    }
+
+    if (consecutivo==true)
+       cout << "SI";
+    else
+       cout << "NO";   
+
+    return 0;
+
+}
