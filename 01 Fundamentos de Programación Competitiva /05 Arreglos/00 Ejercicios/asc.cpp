44febd6aa085a9bb06dcb258862e54eee3fb6b8a
gerardobeltran <contacto@gerardobeltran.mx>
2022-04-30 09:27:46 -0600

    -

---
+++ 01 Fundamentos de Programación Competitiva /05 Arreglos/00 Ejercicios/asc.cpp
@@ -0,0 +1,20 @@
+  #include <iostream>
+using namespace std;
+int asc (const void * a, const void * b){
+return ( *(int*)a - *(int*)b );
+}
+int main(){
+int N, i;
+cout << "Numero de datos a ordenar:";
+cin >> N;
+int datos[N];
+for( i = 0 ; i < N; i++ ) {
+cin >> datos[i];
+}
+qsort(datos, N, sizeof(int), asc); // Función de ordenación
+cout << endl << "Datos Ordenados" << endl;
+for( i = 0 ; i < N; i++ ) {
+cout << datos[i] << " ";
+}
+return 0;
+}
